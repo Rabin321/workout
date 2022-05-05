061d70eee3658dc6a35ad65702c44d70df296ee4
321Rabin <53074799+Rabin321@users.noreply.github.com>
2022-05-05 16:21:59 +0545

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:workout/screens/homescreen.dart';
+import 'package:workout/screens/onboarding.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: OnBoarding(),
     );
   }
 }
